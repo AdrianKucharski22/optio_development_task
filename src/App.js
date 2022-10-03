d57d576cbc19d5640bbe6643ad6cb11d9c49ed8f
AdrianKucharski22 <adrian.kucharski2022@gmail.com>
2022-10-03 12:23:25 +0200

    preparation to work

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      </header>
+      <main>
+          <div className={'squares'}>
+              <div className={'firstSquare'}>
+
+              </div>
+              <div className={'secondSquare'}>
+
+              </div>
+          </div>
+          <div className={'equalAndCircle'}>
+              <div className={'equal'}>
+
+              </div>
+              <div className={'circle'}>
+
+              </div>
+          </div>
+          <div className={'rectangle'}>
+
+          </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
